ef0c973ba905f3f0c3dc94db7ca4810dd5c81bd4
Michelle Patiño <thocokun@gmail.com>
2021-06-09 23:23:07 -0500

    :p

--- pages/index.js
+++ pages/index.js
@@ -14,33 +14,33 @@ export default function Home() {
         <Navbar.Brand href="#home">pull&push</Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link href="#features">Productos</Nav.Link>
+          <Nav.Link href="#pricing">Carrito</Nav.Link>
         </Nav>
         <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-info">Search</Button>
+          <Button variant="outline-info">Buscar</Button>
         </Form>
       </Navbar>
 
       <Container>
         <Form>
           <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
+            <Form.Label>correo electronico</Form.Label>
             <Form.Control type="email" placeholder="Enter email" />
             <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
+            Nunca compartiremos tu correo electrónico con nadie más.
             </Form.Text>
           </Form.Group>
           <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
+            <Form.Label>contraseña</Form.Label>
             <Form.Control type="password" placeholder="Password" />
           </Form.Group>
           <Form.Group controlId="formBasicCheckbox">
             <Form.Check type="checkbox" label="Check me out" />
           </Form.Group>
           <Button variant="primary" type="submit">
-            Submit
+            acceder
           </Button>
         </Form>
       </Container>
